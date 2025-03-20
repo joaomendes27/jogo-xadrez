b75b5dd184a8c5965e0aaa5b79a0e9f7050e1bff
João Mendes <joaovictormendeslima4@gmail.com>
2025-03-20 01:35:10 -0300

    Começando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,10 +7,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p;
-            p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição:" + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
     }
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace tabuleiro
+{
+     class Peca
+    {
+        public Posicao Posicao {  get; set; } 
+        public Cor Cor {  get; protected set; }
+        public int QteMovimentos {get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tabuleiro;
+            QteMovimentos = 0;
+        }
+    }
+}
